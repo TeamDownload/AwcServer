2642c812222acc119749e62897be610497d5c999
nyg159 <nyg159@gmail.com>
2023-05-23 14:12:27 +0900

    init:

--- src/main/java/team/awc1/member/controller/ArduinoDataController.java
+++ src/main/java/team/awc1/member/controller/ArduinoDataController.java
@@ -1,8 +1,10 @@
 package team.awc1.member.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class ArduinoDataController {
 
 }
