dfbf6393544f870b9c8d3e15de0235f10b092341
nyg159 <nyg159@gmail.com>
2023-06-07 22:30:23 +0900

    rename

--- src/main/java/team/awc1/member/service/login/LoginServiceImpl.java
+++ src/main/java/team/awc1/member/service/login/LoginServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class LoginServicelmpls implements LoginService {
+public class LoginServiceImpl implements LoginService {
 
     private final MemberRepository memberRepository;
 
